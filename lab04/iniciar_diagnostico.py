1beff13351b3a8eb7c1153fc12633884a0081ac5
Isao25 <shamir.mantilla@unmsm.edu.pe>
2025-05-01 23:34:22 -0500

    feat:[lab-04] 3cd

--- lab04/iniciar_diagnostico.py
+++ lab04/iniciar_diagnostico.py
@@ -1,9 +1,12 @@
 import clips
 
 env = clips.Environment()
-
 env.load("EXP-diagnostico.clp")
-
 env.reset()
+
+# Añadir esta línea para ver las reglas disponibles
+print("Reglas disponibles:")
+for rule in env.rules():
+    print(rule)
 
 env.call("iniciar-diagnostico")
